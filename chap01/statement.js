e683bda77046a81335e344a9855835149de7cf6e
Kennedy <kennedyotis.nyaga@ingka.ikea.com>
2024-01-20 22:11:15 +0100

    move total amount into function

--- chap01/statement.js
+++ chap01/statement.js
@@ -1,5 +1,4 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   function playFor(aPerformance) {
@@ -53,15 +52,22 @@ export function statement(invoice, plays) {
     return result;
   }
 
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
   for (let perf of invoice.performances) {
     // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
